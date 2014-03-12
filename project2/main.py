0c8944f63e6bf22d4fb34a851c8525155281a9e3
Codeworld <codeworldprodigy@gmail.com>
2014-03-12 16:06:41 +0000

    web second project

---
+++ project2/main.py
@@ -0,0 +1,19 @@
+"""`main` is the top level module for your Flask application."""
+
+# Import the Flask Framework
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.route('/memorygame')
+def catwalk(name=None):
+    return render_template('cards.html',name=name)
